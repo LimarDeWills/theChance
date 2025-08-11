c634e215ba2aac86ac1ecc035d8a730f57723f22
Mikhail Kharitonov <m.kharitonov@postgrespro.ru>
2025-08-11 14:37:03 +0300

    minimal SDL2 window with event loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,50 @@
+#include <SDL.h>
 #include <iostream>
 
-int main()
-{
-    std::cout << "theChance ready" << std::endl;
-    return 1;
+int main(int, char**) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0) {
+        std::cerr << "SDL_Init error: " << SDL_GetError() << "\n";
+        return 1;
+    }
+
+    SDL_Window* win = SDL_CreateWindow(
+        "theChance",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        800, 600,
+        SDL_WINDOW_SHOWN
+    );
+    if (!win) {
+        std::cerr << "SDL_CreateWindow error: " << SDL_GetError() << "\n";
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* ren = SDL_CreateRenderer(
+        win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+    if (!ren) {
+        std::cerr << "SDL_CreateRenderer error: " << SDL_GetError() << "\n";
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    while (running) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) running = false;
+            if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) running = false;
+        }
+
+        SDL_SetRenderDrawColor(ren, 20, 20, 20, 255);
+        SDL_RenderClear(ren);
+
+        SDL_RenderPresent(ren);
+    }
+
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    return 0;
 }
